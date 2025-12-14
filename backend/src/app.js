a9a5badecaf96460b42b884eb59750e72deada64
Abhijeet Negi <abhijeetnegi.ab@gmail.com>
2025-12-14 12:06:40 +0530

    initital

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+//create server
+const express=require ('express');
+const app=express();
+const authRoutes=require('./routes/auth.routes');
+const foodRoutes=require('./routes/food.routes');
+const cookieParser=require('cookie-parser')
+app.use(cookieParser());
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.send("hello world");
+})
+app.use('/api/auth',authRoutes);
+app.use('/api/food',foodRoutes);
+module.exports=app;
